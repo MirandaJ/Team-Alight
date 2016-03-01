63f393e31bf4e351ffeb35699de3851b45af19be
sina-sid <sinas@andrew.cmu.edu>
2016-03-01 18:11:52 -0500

    Incomplete platformer alpha build uploaded

---
+++ Grappling Heron/Assets/HeadMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadMovement : MonoBehaviour {
+
+	public bool facingRight = true;
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void FixedUpdate () {
+		if(Input.GetKeyDown(KeyCode.A)) {
+			Flip (); }
+			
+		if (Input.GetKeyDown(KeyCode.D)) {
+			Flip (); }
+
+		if (Input.GetMouseButtonDown(0)) {
+			Grapple ();
+		}
+	}
+
+	void Grapple () {
+		//Debug.Log(transform.position.x - Input.mousePosition.x);
+		if (System.Math.Abs(transform.position.x - Input.mousePosition.x) < 250f) {
+			if (System.Math.Abs(transform.position.y - Input.mousePosition.y) < 250f) {
+				Vector2 angle = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				transform.position =  Vector2.MoveTowards(transform.position, angle, 5f);
+			}
+		}
+
+	}
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+	}
+}
---
+++ Grappling Heron/Assets/heron_movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heron_movement : MonoBehaviour {
+
+	public float speed = 5f;
+	public bool facingRight = true;
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void FixedUpdate () {
+		if(Input.GetKeyDown(KeyCode.A)) {
+			Flip ();
+			//anim.SetTrigger("WalkLeft");
+			Vector2 move = new Vector2 (-10f,0f);
+			rb2d.AddForce(move*speed); }
+			
+		if (Input.GetKeyDown(KeyCode.D)) {
+			Flip ();
+			Vector2 move = new Vector2 (10f, 0f);
+			rb2d.AddForce(move*speed); }
+	}
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+	}
+}
